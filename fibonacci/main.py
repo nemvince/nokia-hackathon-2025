71c97c6211d3b9a0e84f48d14a91d4d8e1e41fe4
Tamas Vince <nemvince@proton.me>
2025-05-23 00:13:41 +0200

    feat: solve fibonacci

--- fibonacci/main.py
+++ fibonacci/main.py
@@ -1,4 +1,36 @@
-with open('./input.txt', 'r') as f:
-  input = f.read()
+#!/usr/bin/env python3
 
-print(input)
+# Date:     2025-05-22
+# Author:   Tamas Vince
+# Purpose:  Generate a list of Fibonacci numbers divisible by 3 up to a given number N.
+# Runtime:  18.6ms ± 2.9ms over 1000 runs (tested with hyperfine on ThinkPad T480)
+
+# using lru_cache here seems to result in worse performance
+def fibonacci_up_to(N):
+    fib_numbers = []
+    a, b = 0, 1
+    while a <= N:
+        fib_numbers.append(a)
+        a, b = b, a + b
+    return fib_numbers
+
+def solve(line):
+    try:
+        N = int(line.strip())
+    except ValueError:
+        return "N/A"
+
+    fib_numbers = fibonacci_up_to(N)
+    filtered = [x for x in fib_numbers if x % 3 == 0]
+    
+    return "N/A" if not filtered else ", ".join(map(str, filtered))
+
+def main():
+    with open("input.txt", "r") as f:
+        lines = f.readlines()
+    
+    for line in lines:
+        print(solve(line))
+
+if __name__ == "__main__":
+    main()
